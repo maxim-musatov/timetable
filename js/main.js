63f2ef7aa70c69f7b69b9a0cce4ec4e0d99fec45
TheMaxON <re7ted@gmail.com>
2023-01-23 19:26:47 +0200

    old task system

--- js/main.js
+++ js/main.js
@@ -8,60 +8,150 @@ const tasksList = document.querySelector(".modal__card-lesson-task-list");
 const monday = {
 	name: "Понедельник",
 	lessonList: [
-		"Громадська ек. діяльність",
-		"Укр. Мова",
-		"Англ. Мова",
-		"Біологія",
+		{
+			name: "Громадська ек. діяльність",
+			tasks: ["сделать конспект", "переписать параграф"],
+		},
+		{
+			name: "Укр. Мова",
+			tasks: [
+				"выполнить задание в конце конспекта",
+				"переписать параграф",
+			],
+		},
+		{
+			name: "Англ. Мова",
+			tasks: [
+				"выполнить задание в конце конспекта",
+				"переписать параграф",
+			],
+		},
+		{
+			name: "Біологія",
+			tasks: [],
+		},
 	],
-	taskList: {
-		ged: ["сделать конспект", "переписать параграф"],
-		ukrLang: ["выполнить задание в конце конспекта", "переписать параграф"],
-	},
 	accentColor: "#4a6be0",
 };
 
 const tuesday = {
 	name: "Вторник",
-	lessonList: ["Біометрія", "Біометрія", "Фізика"],
-	taskList: {
-		ged: ["сделать конспект", "переписать параграф"],
-		ukrLang: ["выполнить задание в конце конспекта", "переписать параграф"],
-	},
+	lessonList: [
+		{
+			name: "Фізика",
+			tasks: ["сделать конспект", "переписать параграф"],
+		},
+		{
+			name: "Укр. Мова",
+			tasks: [
+				"выполнить задание в конце конспекта",
+				"переписать параграф",
+			],
+		},
+		{
+			name: "Англ. Мова",
+			tasks: [
+				"выполнить задание в конце конспекта",
+				"переписать параграф",
+			],
+		},
+		{
+			name: "Біологія",
+			tasks: [],
+		},
+	],
 	accentColor: "rgb(229, 161, 51)",
 };
 
 const wednesday = {
 	name: "Среда",
-	lessonList: ["Біометрія", "Біометрія", "Фізика"],
-	taskList: {
-		ged: ["сделать конспект", "переписать параграф"],
-		ukrLang: ["выполнить задание в конце конспекта", "переписать параграф"],
-	},
+	lessonList: [
+		{
+			name: "Громадська ек. діяльність",
+			tasks: ["сделать конспект", "переписать параграф"],
+		},
+		{
+			name: "Укр. Мова",
+			tasks: [
+				"выполнить задание в конце конспекта",
+				"переписать параграф",
+			],
+		},
+		{
+			name: "Англ. Мова",
+			tasks: [
+				"выполнить задание в конце конспекта",
+				"переписать параграф",
+			],
+		},
+		{
+			name: "Біологія",
+			tasks: [],
+		},
+	],
 	accentColor: "rgb(187, 83, 86)",
 };
 
 const thursday = {
 	name: "Четверг",
-	lessonList: ["Біометрія", "Біометрія", "Фізика"],
-	taskList: {
-		ged: ["сделать конспект", "переписать параграф"],
-		ukrLang: ["выполнить задание в конце конспекта", "переписать параграф"],
-	},
+	lessonList: [
+		{
+			name: "Громадська ек. діяльність",
+			tasks: ["сделать конспект", "переписать параграф"],
+		},
+		{
+			name: "Укр. Мова",
+			tasks: [
+				"выполнить задание в конце конспекта",
+				"переписать параграф",
+			],
+		},
+		{
+			name: "Англ. Мова",
+			tasks: [
+				"выполнить задание в конце конспекта",
+				"переписать параграф",
+			],
+		},
+		{
+			name: "Біологія",
+			tasks: [],
+		},
+	],
 	accentColor: "rgb(94, 162, 113)",
 };
 
 const friday = {
 	name: "Пятница",
-	lessonList: ["Біометрія", "Біометрія", "Фізика"],
-	taskList: {
-		ged: ["сделать конспект", "переписать параграф"],
-		ukrLang: ["выполнить задание в конце конспекта", "переписать параграф"],
-	},
+	lessonList: [
+		{
+			name: "Громадська ек. діяльність",
+			tasks: ["сделать конспект", "переписать параграф"],
+		},
+		{
+			name: "Укр. Мова",
+			tasks: [
+				"выполнить задание в конце конспекта",
+				"переписать параграф",
+			],
+		},
+		{
+			name: "Англ. Мова",
+			tasks: [
+				"выполнить задание в конце конспекта",
+				"переписать параграф",
+			],
+		},
+		{
+			name: "Біологія",
+			tasks: [],
+		},
+	],
 	accentColor: "rgb(119, 132, 131)",
 };
 
 //MODAL OPEN/LOAD INFO
-const openModal = function (dayName, taskName) {
+const openModal = function (dayName) {
 	const modalDayName = document.querySelector(".modal__card-header-name");
 	const modalPendingTasks = document.querySelector(".modal__card-pending");
 
@@ -70,31 +160,38 @@ const openModal = function (dayName, taskName) {
 		modalCard.classList.add("modal__card-anim");
 	};
 
-	const calculatePendingTasks = dayName.taskList.ged.concat(
-		dayName.taskList.ukrLang
-	).length;
-	modalPendingTasks.textContent = `${calculatePendingTasks} невыполненых задания`;
+	// const calculatePendingTasks = dayName.taskList.ged.concat(
+	// 	dayName.taskList.ukrLang
+	// ).length;
+	// modalPendingTasks.textContent = `${calculatePendingTasks} невыполненых задания`;
 
 	const lessonsListing = function () {
 		for (const lesson of dayName.lessonList) {
 			const createModalLesson = document.createElement("li");
+			const createModalTaskContainer = document.createElement("ul");
 			const createModalTask = document.createElement("li");
 			createModalLesson.className = "modal__card-lesson";
-			createModalTask.className = "modal__card-lesson-task";
-			createModalLesson.textContent = lesson;
-			createModalLesson.textContent = lesson;
+			createModalLesson.textContent = lesson.name;
 			lessonsList.appendChild(createModalLesson);
+			lessonsList.appendChild(createModalTaskContainer);
+			if (lesson.tasks.length !== 0) {
+				createModalTaskContainer.className =
+					"modal__card-lesson-task-container";
+				createModalTask.innerHTML = lesson.tasks.join(";<br>");
+				createModalTask.className = "modal__card-lesson-task";
+				createModalTaskContainer.appendChild(createModalTask);
+			}
 		}
 	};
 
-	const tasksListing = function () {
-		for (const task of dayName.taskList.taskName) {
-			const createModalTask = document.createElement("li");
-			createModalTask.className = "modal__card-lesson-task";
-			createModalTask.textContent = task;
-			tasksList.appendChild(createModalTask);
-		}
-	};
+	// const tasksListing = function () {
+	// 	for (const task of dayName.lessonList) {
+	// 		const createModalTask = document.createElement("li");
+	// 		createModalTask.className = "modal__card-lesson-task";
+	// 		createModalTask.textContent = task.tasks;
+	// 		tasksList.appendChild(createModalTask);
+	// 	}
+	// };
 
 	modal.style.display = "flex";
 	modalDayName.textContent = dayName.name;
